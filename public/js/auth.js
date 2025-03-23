d5b32cfe2a24d6a6dd4fec4374a0764f719c66be
Swayam Nayak <154440440+swayam-2003@users.noreply.github.com>
2025-03-23 21:40:26 +0530

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,8 +2,8 @@
 const firebaseConfig = {
     apiKey: "AIzaSyAT5H3XJ2UXiIxXY4cv5Qi-blfEhd3g0kI",
     authDomain: "certificategen-a1674.firebaseapp.com",
-    projectId: "certificategen-a1674",
-    storageBucket: "certificategen-a1674.firebasestorage.app",
+    projectId: "//,
+    storageBucket: "/-a1674.firebasestorage.app",
     messagingSenderId: "723848073104",
     appId: "1:723848073104:web:5129247306262e2d22ed15"
 };
